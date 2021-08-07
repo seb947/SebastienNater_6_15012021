a8e5b508c2a6e6b11e217f993f440bf4321f7abb
Sebastien NATER <nater.sebastien@gmail.com>
2021-08-07 22:34:28 +0200

    adding first sauce controllers

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 
-//const saucesRoutes = require('./routes/sauce');
+const saucesRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 mongoose.connect(process.env.DB_CREDENTIALS,
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-//app.use('/api/sauces', saucesRoutes);
+app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
 
